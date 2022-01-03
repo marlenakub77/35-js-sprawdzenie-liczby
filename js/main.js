0b736ed6e7c9279f4bd8fbb1b3f0d06b91ef16e1
marlenakub <marlena.kubicka@wp.pl>
2022-01-03 14:54:57 +0100

    35-js-sprawdzenie-liczby-skonczone

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let numN = 130;
+
+function score(number) {
+    if (number > 100) {
+        if (number < 200) {
+            console.log("Liczba znajduje się w przedziale")  
+        }
+        else 
+         console.log("Liczba nie znajduje się w przedziale")
+    }
+    else
+    console.log("Liczba nie znajduje się w przedziale")
+}
+score(numN);
